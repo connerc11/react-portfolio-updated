98febfe87b4b9a97848122e827932ee0976ab2d1
Conner Cochrane <cccochrane1@knights.ucf.edu>
2021-11-03 13:03:36 -0400

    fixed footer and got the paths working

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,21 +1,22 @@
 import React from "react";
 import PDF from "./Resume 9:29.pdf";
+import './style.css'
 
 function Footer() {
     return (
         <footer className="Footer">
-<ul className="">
-<li className="">
-<i class="fab fa-linkedin-in"></i>
-<a href="https://www.linkedin.com/in/conner-cochrane-5a42a8211/"></a>
+<ul className="icon-styles">
+<li className="icon">
+
+<a href="https://www.linkedin.com/in/conner-cochrane-5a42a8211/" alt="linkedn"> <i class="fab fa-linkedin-in"></i></a> 
 </li>
-<li className="">
-<i class="fab fa-github"></i>
-<a href="https://github.com/connerc11"></a>
+<li className="icon">
+
+<a href="https://github.com/connerc11" alt="github"> <i class="fab fa-github"></i></a>
 </li>
-<li className="">
-<i class="far fa-file"></i>
-<a href={PDF}></a>
+<li className="icon">
+
+<a href={PDF} alt="resume"> <i class="far fa-file"></i></a>
 </li>
 </ul>
         
