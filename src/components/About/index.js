ff591f3044e4a6ac7f4b821348cd50fa265effb3
Conner Cochrane <cccochrane1@knights.ucf.edu>
2021-11-07 12:26:44 -0500

    updating styling for contact information

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,11 +4,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function About() {
     return (
-      <div class="jumbotron jumbotron-fluid">
-  <div class="container">
+      <div className="jumbotron jumbotron-fluid" id="container-about">
+  <div className="container">
+  <h1 id="one" className="display-4 text-center">About Me</h1>
   <img src={coverImage} className="profile" alt="profile-pic" />
-    <h1 class="display-4 text-center">Conners Portfolio!</h1>
-    <p class="lead">My name is Conner and this is a place to learn more about me. I am currently 21 years of age and I am in my final semester at the University of Central Florida. I am a very big fan of fishing, football and hockey, and I do enjoy editing and designing videos in my spare time. I am receiving a bachelors degree from UCF in Health Sciences Pre-Clinical. My favorite class that I have ever taken has been Organic Chemistry 2, and I also have a love for Anatomy. I am currently taking an online bootcamp in Software Development from the University of Central Florida. Feel free to reach out ... </p>
+  <br></br>
+  <br></br>
+    
+    <p id="two" className="lead">My name is Conner and this is a place to learn more about me. I am currently 21 years of age and I am in my final semester at the University of Central Florida. I am a very big fan of fishing, football and hockey, and I do enjoy editing and designing videos in my spare time. I am receiving a bachelors degree from UCF in Health Sciences Pre-Clinical. My favorite class that I have ever taken has been Organic Chemistry 2, and I also have a love for Anatomy. I am currently taking an online bootcamp in Software Development from the University of Central Florida. Feel free to reach out ... </p>
   </div>
 </div>
       //   <div className="bio my-5">
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { validateEmail } from "../../utils/helpers";
+import "./form.css"
 
 
 
@@ -43,23 +44,23 @@ function submitHandle(e){
   };
 
 return (
-    <section>
-            <h2> Contact information!</h2>
-            <p>Email: cccochrane1@knights.ucf.edu</p>
-            <p>Phone: 954-918-3334</p>
+    <div className="form-box">
+            <h2 className="form-step" style={{margin: "15px"}}> Contact information</h2>
+            <p className="text-center" style={{color: "blue"}}>Email: cccochrane1@knights.ucf.edu</p>
+            <p className="text-center" style={{color: "blue"}}>Phone: 954-918-3334</p>
 
         <form id="contact-form" onChange={submitHandle}>
-        <div>
-            <label htmlFor="name"> Enter Your Name:</label>
-            <input type="text" onChange={changeHandle} defaultValue={name} name="name" ></input>
+        <div className="field11" >
+            <label htmlFor="name" style={{color: "royalblue"}}> Enter Your Name:</label>
+            <input placeholder="Name here!"type="text" onChange={changeHandle} defaultValue={name} name="name" ></input>
         </div>
-        <div>
+        <div className="text-center" style={{color: "royalblue"}}>
             <label htmlFor="email"> Enter Your Email:</label>
-            <input type="email" onChange={changeHandle} defaultValue={email} name="email" ></input>
+            <input placeholder="Email here!"type="email" onChange={changeHandle} defaultValue={email} name="email" ></input>
         </div>
-        <div>
+        <div className="text-center" style={{color: "royalblue"}}>
             <label htmlFor="message"> Please leave a message:</label>
-            <textarea name="message" onChange={changeHandle} rows="8" defaultValue={message}/>
+            <textarea placeholder="Message here!" name="message" onChange={changeHandle} rows="5" defaultValue={message}/>
         </div>
 
         {errorMessage && (
@@ -67,11 +68,19 @@ return (
             <p className="error-text">{errorMessage}</p>
           </div>
         )}
+      <br></br>
+      <button id="submitBtn" className="submitBtn" type="submit">Send Now!</button>
+      <br></br>
+      
+      <br></br>
+      
 
-        <button type="submit">Send Now!</button>
+
+    
+        
 
 </form>
-</section>
+</div>
 )
 
 
